cc71ae564fcc422df89a0196132b48b41fd46503
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-10-05 23:45:06 +0900

    - [downward] use ExternalProject instead of mk to reduce build failure
    - [downward] add test for planning
    
    - [travis.yml] fix: add EXTRA_DEB=time for test downward

---
+++ 3rdparty/downward/test/test_downward.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
+
+from __future__ import print_function
+import os
+import commands
+import time
+import sys
+import unittest
+from optparse import OptionParser
+import rospy
+
+
+class TestDownward(unittest.TestCase):
+    def test_plan(self):
+        self.assertTrue(os.path.isfile(DOMAIN))
+        domain = DOMAIN
+        self.assertTrue(os.path.isfile(PROBLEM))
+        problem = PROBLEM
+        config = "ipc seq-sat-fd-autotune-1"
+        self.assertIsInstance(PLAN, str)
+        plan_path = PLAN
+        cmd = "rosrun downward plan %s %s %s --plan-file %s" % (domain,
+                                                                problem,
+                                                                config,
+                                                                plan_path)
+        print("EXECUTING: ", cmd, file=sys.stderr)
+        (status, output) = commands.getstatusoutput(cmd)
+        print("DOWNWARD STATUS: %d, OUTPUT: %s" % (status, output), file=sys.stderr)
+        self.assertEqual(status, 0)
+
+        plan_real_path = plan_path + ".1"
+        self.assertTrue(os.path.isfile(plan_real_path))
+
+        with open(REF) as f_ref:
+            ref_lines = f_ref.readlines()
+        with open(plan_real_path) as f_plan:
+            plan_lines = f_plan.readlines()
+        self.assertSequenceEqual(ref_lines, plan_lines)
+
+if __name__ == '__main__':
+    ARGV = rospy.myargv(argv=sys.argv)
+    DOMAIN=ARGV[1]
+    PROBLEM=ARGV[2]
+    PLAN=ARGV[3]
+    REF=ARGV[4]
+    import rostest
+    rostest.rosrun("downward", "test_downward", TestDownward)
