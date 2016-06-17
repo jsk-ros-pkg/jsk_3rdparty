3952056e79d77e85ec8f4805244faf3502c6b504
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-06-17 22:07:18 +0900

    update env-hooks

--- bayesian_network/cmake/InstallBNLearn.cmake
+++ bayesian_network/cmake/InstallBNLearn.cmake
@@ -7,6 +7,8 @@ if(NOT R_FOUND)
 endif()
 
 function(list_r_installed_packages R_INSTALLED_PACKAGES_VAR)
+  set(R_LIB_PATH "${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_LIB_DESTINATION}/R/site-library")
+  set(ENV{R_LIBS} "${R_LIB_PATH}:$ENV{R_LIBS}")
   set(R_LIST_PACKAGES_SCRIPT "${PROJECT_SOURCE_DIR}/cmake/list_packages.r")
   execute_process(
     COMMAND ${R_LIST_PACKAGES_SCRIPT}
@@ -26,6 +28,12 @@ function(find_r_package R_PKG_NAME)
 endfunction()
 
 function(install_r_package R_PKG_NAME)
+  set(R_LIB_PATH "${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_LIB_DESTINATION}/R/site-library")
+  set(ENV{R_LIBS} "${R_LIB_PATH}:$ENV{R_LIBS}")
+  message(STATUS "Appending to R_LIBS: ${R_LIB_PATH}")
+  if(NOT EXISTS ${R_LIB_PATH})
+    file(MAKE_DIRECTORY ${R_LIB_PATH})
+  endif()
   set(R_INSTALL_PACKAGE_SCRIPT "${PROJECT_SOURCE_DIR}/cmake/install_package.r")
   execute_process(
     COMMAND ${R_INSTALL_PACKAGE_SCRIPT} ${R_PKG_NAME}
--- bayesian_network/env-hooks/99.bayesian_network.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "@DEVELSPACE@" = "True" -o "@DEVELSPACE@" = "true" ]; then
-  export R_LIBS=@CATKIN_DEVEL_PREFIX@/lib/R/site-library:$R_LIBS
-else
-  export R_LIBS=@CATKIN_INSTALL_PREFIX@/lib/R/site-library:$R_LIBS
-fi
