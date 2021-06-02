f8de3bdd30bd831515cb1179b22a1116e95d4f81
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-06-02 21:48:32 +0900

    src/chaplus_ros directory is not exists for now

--- chaplus_ros/setup.py
+++ chaplus_ros/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['chaplus_ros'],
-    package_dir={'':'src'},
+    # package_dir={'':'src'},
     scripts=['scripts/chaplus_ros.py']
 )
 
