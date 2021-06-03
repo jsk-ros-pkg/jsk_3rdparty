d07fb0018642c533db96597f48da0266f8b3de85
Koki Shinjo <sktometometo@gmail.com>
2021-06-03 17:08:30 +0900

    [webrtcvad_ros] fix bugs

--- webrtcvad_ros/node_scripts/webrtcvad_ros.py
+++ webrtcvad_ros/node_scripts/webrtcvad_ros.py
@@ -3,20 +3,21 @@
 import webrtcvad
 import rospy
 from audio_common_msgs.msg import AudioData, AudioInfo
+from std_msgs.msg import Bool
 
 class WebRTCVADROS(object):
 
     def __init__(self):
 
         self._current_speaking = False
-        self._speech_audio_buffer = []
+        self._speech_audio_buffer = ''
 
         aggressiveness = rospy.get_param('~aggressiveness',1)
         self._vad = webrtcvad.Vad(int(aggressiveness))
 
         self._pub_is_speech = rospy.Publisher('~is_speech',Bool,queue_size=1)
         self._pub_speech_audio = rospy.Publisher('~speech_audio',AudioData,queue_size=1)
-        self._pub_speech_audio_info = rospy.Publisher('~speech_audio_info',AudioData,queue_size=1,latch=True)
+        self._pub_speech_audio_info = rospy.Publisher('~speech_audio_info',AudioInfo,queue_size=1,latch=True)
 
         self._audio_info = rospy.wait_for_message('audio_info',AudioInfo)
         if self._audio_info.sample_format != 'S16LE':
@@ -30,19 +31,18 @@ class WebRTCVADROS(object):
         self._sub = rospy.Subscriber('audio_data',AudioData,self._callback)
 
     def _callback(self, msg):
-        is_speech = vad.is_speech(msg.data, self._audio_info.sample_rate)
+        is_speech = self._vad.is_speech(msg.data, self._audio_info.sample_rate)
         self._pub_is_speech.publish(Bool(is_speech))
-
         if self._current_speaking == True and is_speech == True:
-            self._speech_audio_buffer.append(msg.data)
+            self._speech_audio_buffer = self._speech_audio_buffer + msg.data
         elif self._current_speaking == False and is_speech == True:
-            self._speech_audio_buffer.append(msg.data)
+            self._speech_audio_buffer = msg.data
             self._current_speaking = True
         elif self._current_speaking == True and is_speech == False:
-            self._speech_audio_buffer.append(msg.data)
+            self._speech_audio_buffer = self._speech_audio_buffer + msg.data
             self._pub_speech_audio.publish(AudioData(self._speech_audio_buffer))
             self._current_speaking = False
-            self._speech_audio_buffer = []
+            self._speech_audio_buffer = ''
 
 def main():
 
