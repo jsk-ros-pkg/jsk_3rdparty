25bd3e5332092ded68767ab6e835d8d9a0bb605c
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-06-10 08:54:13 +0900

    fix   error: package directory 'chaplus_ros' does not exist

--- chaplus_ros/setup.py
+++ chaplus_ros/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['chaplus_ros'],
+    # packages=['chaplus_ros'],
     # package_dir={'':'src'},
     scripts=['scripts/chaplus_ros.py']
 )
