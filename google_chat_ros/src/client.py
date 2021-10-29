bdeb2c1abf5164db76d09095f94ffd43539b8a59
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2021-10-29 17:51:35 +0900

    add actionmsg, catkin install, ros action

--- google_chat_ros/src/client.py
+++ google_chat_ros/src/client.py
@@ -7,27 +7,24 @@ from httplib2 import Http
 import json
 from oauth2client.service_account import ServiceAccountCredentials
 
-class GoogleChatClient(object, keyfile):
-    def __init__(self):
+class GoogleChatClient():
+    def __init__(self, keyfile):
         self._auth_scopes = "https://www.googleapis.com/auth/chat.bot"
         self.keyfile = keyfile
         self.__credentials = None
         self._chat = None
-        self.res = None
 
-    def authenticate(self):
-        """Authenticate Google REST API by json key file.
+    def build_service(self):
+        """Authenticate Google REST API and start service by json key file.
         Please see https://developers.google.com/chat/how-tos/service-accounts#step_1_create_service_account_and_private_key for details.
         :param keyfile_path: str, the file path of json key file
         """
         self.__credentials = ServiceAccountCredentials.from_json_keyfile_name(self.keyfile, self._auth_scopes)
-
-    def build_service(self):
         self._chat = build('chat', 'v1', http=self.__credentials.authorize(Http()))
         
     def send_text(self, space, text):
         parent = 'spaces/' + space
-        self.res = self._chat.spaces().messages().create(parent=parent, body={'text':text}).execute() # returns same 403 error both authenticate error and not connected error
+        return self._chat.spaces().messages().create(parent=parent, body={'text':text}).execute() # returns same 403 error both authenticate error and not connected error
 
     def send_card(self, space, content):
         pass
