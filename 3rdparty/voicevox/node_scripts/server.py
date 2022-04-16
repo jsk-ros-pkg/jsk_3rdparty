cf949de929ae6939d73c4c22a78c4bcd997e80ee
iory <ab.ioryz@gmail.com>
2022-04-17 05:19:15 +0900

    Add voicevox (AI speech synthesis) package

---
+++ 3rdparty/voicevox/node_scripts/server.py
@@ -0,0 +1,567 @@
+#!/usr/bin/env python
+
+# This code was created based on the following link's code.
+# https://github.com/VOICEVOX/voicevox_engine/blob/0.11.4/run.py
+
+import base64
+from distutils.version import LooseVersion
+from functools import lru_cache
+import imp
+import json
+import multiprocessing
+import os.path as osp
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from tempfile import TemporaryFile
+from typing import Dict
+from typing import List
+from typing import Optional
+import zipfile
+
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.params import Query
+from fastapi import Response
+import rospy
+import rospkg
+import soundfile
+from starlette.responses import FileResponse
+import uvicorn
+
+
+abs_path = osp.dirname(osp.abspath(__file__))
+voicevox_engine = imp.load_package(
+    'voicevox_engine', osp.join(abs_path, 'voicevox_engine/voicevox_engine'))
+
+
+from voicevox_engine import __version__
+from voicevox_engine.kana_parser import create_kana
+from voicevox_engine.kana_parser import parse_kana
+from voicevox_engine.model import AccentPhrase
+from voicevox_engine.model import AudioQuery
+from voicevox_engine.model import ParseKanaBadRequest
+from voicevox_engine.model import ParseKanaError
+from voicevox_engine.model import Speaker
+from voicevox_engine.model import SpeakerInfo
+from voicevox_engine.model import SupportedDevicesInfo
+from voicevox_engine.morphing import \
+    synthesis_morphing_parameter as _synthesis_morphing_parameter
+from voicevox_engine.morphing import synthesis_morphing
+from voicevox_engine.preset import Preset
+from voicevox_engine.preset import PresetLoader
+from voicevox_engine.synthesis_engine import make_synthesis_engines
+from voicevox_engine.synthesis_engine import SynthesisEngineBase
+from voicevox_engine.user_dict import user_dict_startup_processing
+from voicevox_engine.utility import connect_base64_waves
+from voicevox_engine.utility import ConnectBase64WavesException
+from voicevox_engine.utility import engine_root
+
+
+def b64encode_str(s):
+    return base64.b64encode(s).decode("utf-8")
+
+
+def generate_app(
+    synthesis_engines: Dict[str, SynthesisEngineBase], latest_core_version: str
+) -> FastAPI:
+    root_dir = engine_root()
+
+    default_sampling_rate = synthesis_engines[latest_core_version].default_sampling_rate
+
+    app = FastAPI(
+        title="VOICEVOX ENGINE",
+        description="VOICEVOXの音声合成エンジンです。",
+        version=__version__,
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    preset_loader = PresetLoader(
+        preset_path=root_dir / "presets.yaml",
+    )
+
+    # キャッシュを有効化
+    # モジュール側でlru_cacheを指定するとキャッシュを制御しにくいため、HTTPサーバ側で指定する
+    # TODO: キャッシュを管理するモジュール側API・HTTP側APIを用意する
+    synthesis_morphing_parameter = lru_cache(maxsize=4)(_synthesis_morphing_parameter)
+
+    # @app.on_event("startup")
+    # async def start_catch_disconnection():
+    #     if args.enable_cancellable_synthesis:
+    #         loop = asyncio.get_event_loop()
+    #         _ = loop.create_task(cancellable_engine.catch_disconnection())
+
+    @app.on_event("startup")
+    def apply_user_dict():
+        user_dict_startup_processing()
+
+    def get_engine(core_version: Optional[str]) -> SynthesisEngineBase:
+        if core_version is None:
+            return synthesis_engines[latest_core_version]
+        if core_version in synthesis_engines:
+            return synthesis_engines[core_version]
+        raise HTTPException(status_code=422, detail="不明なバージョンです")
+
+    @app.post(
+        "/audio_query",
+        response_model=AudioQuery,
+        tags=["クエリ作成"],
+        summary="音声合成用のクエリを作成する",
+    )
+    def audio_query(text: str, speaker: int, core_version: Optional[str] = None):
+        """
+        クエリの初期値を得ます。ここで得られたクエリはそのまま音声合成に利用できます。各値の意味は`Schemas`を参照してください。
+        """
+        engine = get_engine(core_version)
+        accent_phrases = engine.create_accent_phrases(text, speaker_id=speaker)
+        return AudioQuery(
+            accent_phrases=accent_phrases,
+            speedScale=1,
+            pitchScale=0,
+            intonationScale=1,
+            volumeScale=1,
+            prePhonemeLength=0.1,
+            postPhonemeLength=0.1,
+            outputSamplingRate=default_sampling_rate,
+            outputStereo=False,
+            kana=create_kana(accent_phrases),
+        )
+
+    @app.post(
+        "/audio_query_from_preset",
+        response_model=AudioQuery,
+        tags=["クエリ作成"],
+        summary="音声合成用のクエリをプリセットを用いて作成する",
+    )
+    def audio_query_from_preset(
+        text: str, preset_id: int, core_version: Optional[str] = None
+    ):
+        """
+        クエリの初期値を得ます。ここで得られたクエリはそのまま音声合成に利用できます。各値の意味は`Schemas`を参照してください。
+        """
+        engine = get_engine(core_version)
+        presets, err_detail = preset_loader.load_presets()
+        if err_detail:
+            raise HTTPException(status_code=422, detail=err_detail)
+        for preset in presets:
+            if preset.id == preset_id:
+                selected_preset = preset
+                break
+        else:
+            raise HTTPException(status_code=422, detail="該当するプリセットIDが見つかりません")
+
+        accent_phrases = engine.create_accent_phrases(
+            text, speaker_id=selected_preset.style_id
+        )
+        return AudioQuery(
+            accent_phrases=accent_phrases,
+            speedScale=selected_preset.speedScale,
+            pitchScale=selected_preset.pitchScale,
+            intonationScale=selected_preset.intonationScale,
+            volumeScale=selected_preset.volumeScale,
+            prePhonemeLength=selected_preset.prePhonemeLength,
+            postPhonemeLength=selected_preset.postPhonemeLength,
+            outputSamplingRate=default_sampling_rate,
+            outputStereo=False,
+            kana=create_kana(accent_phrases),
+        )
+
+    @app.post(
+        "/accent_phrases",
+        response_model=List[AccentPhrase],
+        tags=["クエリ編集"],
+        summary="テキストからアクセント句を得る",
+        responses={
+            400: {
+                "description": "読み仮名のパースに失敗",
+                "model": ParseKanaBadRequest,
+            }
+        },
+    )
+    def accent_phrases(
+        text: str,
+        speaker: int,
+        is_kana: bool = False,
+        core_version: Optional[str] = None,
+    ):
+        """
+        テキストからアクセント句を得ます。
+        is_kanaが`true`のとき、テキストは次のようなAquesTalkライクな記法に従う読み仮名として処理されます。デフォルトは`false`です。
+        * 全てのカナはカタカナで記述される
+        * アクセント句は`/`または`、`で区切る。`、`で区切った場合に限り無音区間が挿入される。
+        * カナの手前に`_`を入れるとそのカナは無声化される
+        * アクセント位置を`'`で指定する。全てのアクセント句にはアクセント位置を1つ指定する必要がある。
+        * アクセント句末に`？`(全角)を入れることにより疑問文の発音ができる。
+        """
+        engine = get_engine(core_version)
+        if is_kana:
+            try:
+                accent_phrases = parse_kana(text)
+            except ParseKanaError as err:
+                raise HTTPException(
+                    status_code=400,
+                    detail=ParseKanaBadRequest(err).dict(),
+                )
+            accent_phrases = engine.replace_mora_data(
+                accent_phrases=accent_phrases, speaker_id=speaker
+            )
+
+            return accent_phrases
+        else:
+            return engine.create_accent_phrases(text, speaker_id=speaker)
+
+    @app.post(
+        "/mora_data",
+        response_model=List[AccentPhrase],
+        tags=["クエリ編集"],
+        summary="アクセント句から音高・音素長を得る",
+    )
+    def mora_data(
+        accent_phrases: List[AccentPhrase],
+        speaker: int,
+        core_version: Optional[str] = None,
+    ):
+        engine = get_engine(core_version)
+        return engine.replace_mora_data(accent_phrases, speaker_id=speaker)
+
+    @app.post(
+        "/mora_length",
+        response_model=List[AccentPhrase],
+        tags=["クエリ編集"],
+        summary="アクセント句から音素長を得る",
+    )
+    def mora_length(
+        accent_phrases: List[AccentPhrase],
+        speaker: int,
+        core_version: Optional[str] = None,
+    ):
+        engine = get_engine(core_version)
+        return engine.replace_phoneme_length(
+            accent_phrases=accent_phrases, speaker_id=speaker
+        )
+
+    @app.post(
+        "/mora_pitch",
+        response_model=List[AccentPhrase],
+        tags=["クエリ編集"],
+        summary="アクセント句から音高を得る",
+    )
+    def mora_pitch(
+        accent_phrases: List[AccentPhrase],
+        speaker: int,
+        core_version: Optional[str] = None,
+    ):
+        engine = get_engine(core_version)
+        return engine.replace_mora_pitch(
+            accent_phrases=accent_phrases, speaker_id=speaker
+        )
+
+    @app.post(
+        "/synthesis",
+        response_class=FileResponse,
+        responses={
+            200: {
+                "content": {
+                    "audio/wav": {"schema": {"type": "string", "format": "binary"}}
+                },
+            }
+        },
+        tags=["音声合成"],
+        summary="音声合成する",
+    )
+    def synthesis(
+        query: AudioQuery,
+        speaker: int,
+        enable_interrogative_upspeak: bool = Query(  # noqa: B008
+            default=True,
+            description="疑問系のテキストが与えられたら語尾を自動調整する",
+        ),
+        core_version: Optional[str] = None,
+    ):
+        engine = get_engine(core_version)
+        wave = engine.synthesis(
+            query=query,
+            speaker_id=speaker,
+            enable_interrogative_upspeak=enable_interrogative_upspeak,
+        )
+
+        with NamedTemporaryFile(delete=False) as f:
+            soundfile.write(
+                file=f, data=wave, samplerate=query.outputSamplingRate, format="WAV"
+            )
+
+        return FileResponse(f.name, media_type="audio/wav")
+
+    @app.post(
+        "/multi_synthesis",
+        response_class=FileResponse,
+        responses={
+            200: {
+                "content": {
+                    "application/zip": {
+                        "schema": {"type": "string", "format": "binary"}
+                    }
+                },
+            }
+        },
+        tags=["音声合成"],
+        summary="複数まとめて音声合成する",
+    )
+    def multi_synthesis(
+        queries: List[AudioQuery],
+        speaker: int,
+        core_version: Optional[str] = None,
+    ):
+        engine = get_engine(core_version)
+        sampling_rate = queries[0].outputSamplingRate
+
+        with NamedTemporaryFile(delete=False) as f:
+
+            with zipfile.ZipFile(f, mode="a") as zip_file:
+
+                for i in range(len(queries)):
+
+                    if queries[i].outputSamplingRate != sampling_rate:
+                        raise HTTPException(
+                            status_code=422, detail="サンプリングレートが異なるクエリがあります"
+                        )
+
+                    with TemporaryFile() as wav_file:
+
+                        wave = engine.synthesis(query=queries[i], speaker_id=speaker)
+                        soundfile.write(
+                            file=wav_file,
+                            data=wave,
+                            samplerate=sampling_rate,
+                            format="WAV",
+                        )
+                        wav_file.seek(0)
+                        zip_file.writestr(f"{str(i + 1).zfill(3)}.wav", wav_file.read())
+
+        return FileResponse(f.name, media_type="application/zip")
+
+    @app.post(
+        "/synthesis_morphing",
+        response_class=FileResponse,
+        responses={
+            200: {
+                "content": {
+                    "audio/wav": {"schema": {"type": "string", "format": "binary"}}
+                },
+            }
+        },
+        tags=["音声合成"],
+        summary="2人の話者でモーフィングした音声を合成する",
+    )
+    def _synthesis_morphing(
+        query: AudioQuery,
+        base_speaker: int,
+        target_speaker: int,
+        morph_rate: float = Query(..., ge=0.0, le=1.0),  # noqa: B008
+        core_version: Optional[str] = None,
+    ):
+        """
+        指定された2人の話者で音声を合成、指定した割合でモーフィングした音声を得ます。
+        モーフィングの割合は`morph_rate`で指定でき、0.0でベースの話者、1.0でターゲットの話者に近づきます。
+        """
+        engine = get_engine(core_version)
+
+        # 生成したパラメータはキャッシュされる
+        morph_param = synthesis_morphing_parameter(
+            engine=engine,
+            query=query,
+            base_speaker=base_speaker,
+            target_speaker=target_speaker,
+        )
+
+        morph_wave = synthesis_morphing(
+            morph_param=morph_param,
+            morph_rate=morph_rate,
+            output_stereo=query.outputStereo,
+        )
+
+        with NamedTemporaryFile(delete=False) as f:
+            soundfile.write(
+                file=f,
+                data=morph_wave,
+                samplerate=morph_param.fs,
+                format="WAV",
+            )
+
+        return FileResponse(f.name, media_type="audio/wav")
+
+    @app.post(
+        "/connect_waves",
+        response_class=FileResponse,
+        responses={
+            200: {
+                "content": {
+                    "audio/wav": {"schema": {"type": "string", "format": "binary"}}
+                },
+            }
+        },
+        tags=["その他"],
+        summary="base64エンコードされた複数のwavデータを一つに結合する",
+    )
+    def connect_waves(waves: List[str]):
+        """
+        base64エンコードされたwavデータを一纏めにし、wavファイルで返します。
+        """
+        try:
+            waves_nparray, sampling_rate = connect_base64_waves(waves)
+        except ConnectBase64WavesException as err:
+            return HTTPException(status_code=422, detail=str(err))
+
+        with NamedTemporaryFile(delete=False) as f:
+            soundfile.write(
+                file=f,
+                data=waves_nparray,
+                samplerate=sampling_rate,
+                format="WAV",
+            )
+
+            return FileResponse(f.name, media_type="audio/wav")
+
+    @app.get("/presets", response_model=List[Preset], tags=["その他"])
+    def get_presets():
+        """
+        エンジンが保持しているプリセットの設定を返します
+
+        Returns
+        -------
+        presets: List[Preset]
+            プリセットのリスト
+        """
+        presets, err_detail = preset_loader.load_presets()
+        if err_detail:
+            raise HTTPException(status_code=422, detail=err_detail)
+        return presets
+
+    @app.get("/version", tags=["その他"])
+    def version() -> str:
+        return __version__
+
+    @app.get("/core_versions", response_model=List[str], tags=["その他"])
+    def core_versions() -> List[str]:
+        return Response(
+            content=json.dumps(list(synthesis_engines.keys())),
+            media_type="application/json",
+        )
+
+    @app.get("/speakers", response_model=List[Speaker], tags=["その他"])
+    def speakers(
+        core_version: Optional[str] = None,
+    ):
+        engine = get_engine(core_version)
+        return Response(
+            content=engine.speakers,
+            media_type="application/json",
+        )
+
+    @app.get("/speaker_info", response_model=SpeakerInfo, tags=["その他"])
+    def speaker_info(speaker_uuid: str, core_version: Optional[str] = None):
+        """
+        指定されたspeaker_uuidに関する情報をjson形式で返します。
+        画像や音声はbase64エンコードされたものが返されます。
+
+        Returns
+        -------
+        ret_data: SpeakerInfo
+        """
+        speakers = json.loads(get_engine(core_version).speakers)
+        for i in range(len(speakers)):
+            if speakers[i]["speaker_uuid"] == speaker_uuid:
+                speaker = speakers[i]
+                break
+        else:
+            raise HTTPException(status_code=404, detail="該当する話者が見つかりません")
+
+        try:
+            policy = (root_dir / f"speaker_info/{speaker_uuid}/policy.md").read_text(
+                "utf-8"
+            )
+            portrait = b64encode_str(
+                (root_dir / f"speaker_info/{speaker_uuid}/portrait.png").read_bytes()
+            )
+            style_infos = []
+            for style in speaker["styles"]:
+                id = style["id"]
+                icon = b64encode_str(
+                    (
+                        root_dir / f"speaker_info/{speaker_uuid}/icons/{id}.png"
+                    ).read_bytes()
+                )
+                voice_samples = [
+                    b64encode_str(
+                        (
+                            root_dir
+                            / "speaker_info/{}/voice_samples/{}_{}.wav".format(
+                                speaker_uuid, id, str(j + 1).zfill(3)
+                            )
+                        ).read_bytes()
+                    )
+                    for j in range(3)
+                ]
+                style_infos.append(
+                    {"id": id, "icon": icon, "voice_samples": voice_samples}
+                )
+        except FileNotFoundError:
+            import traceback
+
+            traceback.print_exc()
+            raise HTTPException(status_code=500, detail="追加情報が見つかりませんでした")
+
+        ret_data = {"policy": policy, "portrait": portrait, "style_infos": style_infos}
+        return ret_data
+
+    @app.get("/supported_devices", response_model=SupportedDevicesInfo, tags=["その他"])
+    def supported_devices(
+        core_version: Optional[str] = None,
+    ):
+        supported_devices = get_engine(core_version).supported_devices
+        if supported_devices is None:
+            raise HTTPException(status_code=422, detail="非対応の機能です。")
+        return Response(
+            content=supported_devices,
+            media_type="application/json",
+        )
+
+    return app
+
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    rospy.init_node('julius_server')
+
+    rospack = rospkg.RosPack()
+    voicevox_dir = osp.join(rospack.get_path('voicevox'), 'lib')
+    voicelib_dir = [Path(voicevox_dir)]
+    use_gpu = False
+    host = rospy.get_param('~host', "127.0.0.1")
+    port = rospy.get_param('~port', 50021)
+    cpu_num_threads = rospy.get_param('~cpu_num_threads', None)
+    if cpu_num_threads is None:
+        cpu_num_threads = multiprocessing.cpu_count()
+
+    synthesis_engines = make_synthesis_engines(
+        use_gpu=use_gpu,
+        voicelib_dirs=voicelib_dir,
+        cpu_num_threads=cpu_num_threads,
+    )
+    if len(synthesis_engines) == 0:
+        rospy.logerr("音声合成エンジンがありません。")
+    latest_core_version = str(max([LooseVersion(ver)
+                                   for ver in synthesis_engines]))
+
+    uvicorn.run(
+        generate_app(synthesis_engines, latest_core_version),
+        host=host,
+        port=port,
+    )
