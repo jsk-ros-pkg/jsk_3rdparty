b7d7b0ac8548ff3f21f79251dafbb08eba42ed72
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-05-14 20:03:07 +0900

    use aques_talk as sound_play plugin

---
+++ 3rdparty/aques_talk/setup.py
@@ -0,0 +1,9 @@
+from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
+
+d = generate_distutils_setup(
+      packages=['aques_talk'],
+      package_dir={'': 'src'}
+      )
+
+setup(**d)
