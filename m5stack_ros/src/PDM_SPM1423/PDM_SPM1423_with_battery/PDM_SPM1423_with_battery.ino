b3a41b84a050df06a2be0ed24dff70afce6c7c42
Naoya Yamaguchi <708yamaguchi@gmail.com>
2022-07-04 20:44:36 +0900

    Update PDM_SPM1423 with battery information sample with common header file

--- m5stack_ros/src/PDM_SPM1423/PDM_SPM1423_with_battery/PDM_SPM1423_with_battery.ino
+++ m5stack_ros/src/PDM_SPM1423/PDM_SPM1423_with_battery/PDM_SPM1423_with_battery.ino
@@ -1,31 +1,29 @@
-#include <m5stack_ros.h>
-
-// I2S microphone
+#include <m5stack_ros_with_battery.h>
 #include <PDM_SPM1423.h>
 #include <audio_common_msgs/AudioData.h>
-#include <std_msgs/Float32.h>
-// I2C battery management module
-#include <IP5306.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Float32.h>
 #include <std_msgs/UInt16.h>
 
+int loop_count = 1000;
+
 audio_common_msgs::AudioData audio_msg;
-ros::Publisher audio_pub("audio", &audio_msg);
+ros::Publisher audio_pub("pdm_audio", &audio_msg);
 std_msgs::Float32 volume_msg;
-ros::Publisher volume_pub("volume", &volume_msg);
+ros::Publisher volume_pub("pdm_volume", &volume_msg);
 
-std_msgs::UInt16 level_msg;
-ros::Publisher level_pub("battery_level", &level_msg);
-std_msgs::Bool charging_msg;
-ros::Publisher charging_pub("is_charging", &charging_msg);
-
-int loop_count = 0;
+void pubBattery() {
+  // Enable I2C
+  measureIP5306();
+  level_msg.data = battery_level;
+  charging_msg.data = isCharging;
+  level_pub.publish(&level_msg);
+  charging_pub.publish(&charging_msg);
+}
 
 void pubAudio() {
-  // Read microphone data
   readMic();
   calcVolume();
-  // Publish ROS topics
   audio_msg.data = microRawData;
   audio_msg.data_length = bytesread;
   audio_pub.publish(&audio_msg);
@@ -35,26 +33,17 @@ void pubAudio() {
   drawVolume(volume);
 }
 
-void pubBattery() {
-  measureIP5306();
-  level_msg.data = battery_level;
-  charging_msg.data = isCharging;
-  level_pub.publish(&level_msg);
-  charging_pub.publish(&charging_msg);
-  nh.spinOnce();
-}
-
 void setup() {
   setupM5stackROS();
-  microPhoneSetup();
-  enableI2C();
   setupIP5306();
-  disableI2C();
+  microPhoneSetup();
   header("PDM Unit", BLACK);
   nh.advertise(audio_pub);
   nh.advertise(volume_pub);
-  nh.advertise(level_pub);
-  nh.advertise(charging_pub);
+  enableI2C();
+  measureIP5306();
+  afterSetup();
+  disableI2C();
 }
 
 void loop() {
@@ -63,13 +52,16 @@ void loop() {
     // Publish battery info while stopping publishing audio info
     // This is because I2C and I2S share the pin and they can't be measured simultaneously
     enableI2C();
-    pubBattery();
+    beforeLoop();
     disableI2C();
     loop_count = 0;
     // Remove noisy audio data right after I2S starts
     delay(200);
   }
-  pubAudio();
+  else {
+    pubAudio();
+  }
   nh.spinOnce();
   loop_count++;
+  delay(30);
 }
--- m5stack_ros/src/PDM_SPM1423/README.md
+++ m5stack_ros/src/PDM_SPM1423/README.md
@@ -25,6 +25,8 @@ Published topics:
   - We recommend Bluetooth or Wi-fi connection because USB connection is slow for some reason.
   - If you connect PDM_SPM1423 microphone to M5Stack Grove connector, you cannot use I2S and I2C devices simultaneously. In that case, try `enableI2C` and `disableI2C` functions defined in [PDM_SPM1423.h](https://github.com/jsk-ros-pkg/jsk_3rdparty/tree/master/m5stack_ros/include/PDM_SPM1423.h).
   - The sample program which uses I2S microphone and I2C battery management module is under [PDM_SPM1423_with_battery](https://github.com/jsk-ros-pkg/jsk_3rdparty/tree/master/m5stack_ros/src/PDM_SPM1423/PDM_SPM1423_with_battery)
+    - When acquiring battery information, the connection to the microphone temporarily stops.
+    - Therefore, there are times when the microphone information is temporarily unavailable
 
 - Run
 
