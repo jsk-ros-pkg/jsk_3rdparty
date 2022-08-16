5744ed474f13a16e471e70274d1931699fcb8e0f
iory <ab.ioryz@gmail.com>
2022-08-16 16:17:59 +0900

    [rostwitter] Enable to tweet with 280(hankaku) characters

--- rostwitter/python/rostwitter/util.py
+++ rostwitter/python/rostwitter/util.py
@@ -1,4 +1,5 @@
 import os
+import unicodedata
 import yaml
 
 import rospy
@@ -22,3 +23,37 @@ def load_oauth_settings(yaml_path):
         akey = key['AKEY']
         asecret = key['ASECRET']
     return ckey, csecret, akey, asecret
+
+
+def count_tweet_text(text):
+    count = 0
+    for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+            count += 2
+        else:
+            count += 1
+    return count
+
+
+def split_tweet_text(text, length=280):
+    texts = []
+    split_text = ''
+    count = 0
+    for c in text:
+        if count == 281:
+            # last word is zenkaku.
+            texts.append(split_text[:-1])
+            split_text = split_text[-1:]
+            count = 2
+        elif count == 280:
+            texts.append(split_text)
+            split_text = ''
+            count = 0
+        split_text += c
+        if unicodedata.east_asian_width(c) in 'FWA':
+            count += 2
+        else:
+            count += 1
+    if count != 0:
+        texts.append(split_text)
+    return texts
