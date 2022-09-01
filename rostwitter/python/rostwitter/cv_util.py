49d88a8fd58e9d7245525cc39c9e276c636c6399
iory <ab.ioryz@gmail.com>
2022-09-01 16:35:46 +0900

    [rostwitter] Assume base64 images are not contiguous without spaces

--- rostwitter/python/rostwitter/cv_util.py
+++ rostwitter/python/rostwitter/cv_util.py
@@ -8,7 +8,7 @@ import numpy as np
 import rospy
 
 
-base64_and_filepath_image_pattern = re.compile(r'((?:/9j/)(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?|/\S+\.(?:jpeg|jpg|png|gif))')
+base64_and_filepath_image_pattern = re.compile(r'((?:/9j/)(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)? ?|/\S+\.(?:jpeg|jpg|png|gif))')
 
 
 def encode_image_cv2(img, quality=90):
@@ -35,28 +35,22 @@ def is_base64_image(b64encoded):
     return True
 
 
-def parse_matched_media(text):
-    media_list = []
+def get_image_from_text(text):
     if base64_and_filepath_image_pattern.match(text) is None:
-        return media_list
-    split_texts = re.split('/9j/', text)
-    split_texts = list(filter(lambda x: x is not None and len(x.strip()) > 0,
-                              split_texts))
-    for text in split_texts:
-        if os.path.exists(text):
-            path = text
-            if imghdr.what(path) in ['jpeg', 'png', 'gif']:
-                with open(path, 'rb') as f:
-                    media_list.append(f.read())
-        else:
-            text = '/9j/' + text
-            succ = is_base64_image(text)
-            if succ:
-                bin = text.split(",")[-1]
-                bin = base64.b64decode(bin)
-                bin = np.frombuffer(bin, np.uint8)
-                media_list.append(bin)
-    return media_list
+        return None
+
+    if os.path.exists(text):
+        path = text
+        if imghdr.what(path) in ['jpeg', 'png', 'gif']:
+            with open(path, 'rb') as f:
+                return f.read()
+    else:
+        succ = is_base64_image(text)
+        if succ:
+            bin = text.split(",")[-1]
+            bin = base64.b64decode(bin)
+            bin = np.frombuffer(bin, np.uint8)
+            return bin
 
 
 def extract_media_from_text(text):
@@ -69,13 +63,14 @@ def extract_media_from_text(text):
     texts = []
     imgs = []
     for text in target_texts:
-        media_list = parse_matched_media(text)
-        if len(media_list) == 0:
+        img = get_image_from_text(text)
+        if img is None:
             split_texts.append(text)
             imgs_list.append(imgs)
             imgs = []
         else:
-            imgs.extend(media_list)
+            imgs.append(img)
+
     if len(imgs) > 0:
         imgs_list.append(imgs)
     if len(split_texts) > 0:
