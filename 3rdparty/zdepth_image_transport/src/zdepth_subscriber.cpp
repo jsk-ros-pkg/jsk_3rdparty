92ae83c8a8719fa9a415e08a0b1a71d05fbb6ac9
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-11-03 02:05:38 +0900

    make decompress copy faster

--- 3rdparty/zdepth_image_transport/src/zdepth_subscriber.cpp
+++ 3rdparty/zdepth_image_transport/src/zdepth_subscriber.cpp
@@ -1,5 +1,6 @@
 // #include <cv_bridge/cv_bridge.h>
 
+#include <chrono>
 #include <ros/console.h>
 #include "zdepth/zdepth.hpp"
 #include "zdepth_image_transport/zdepth_subscriber.h"
@@ -23,22 +24,26 @@ namespace zdepth_image_transport
     int width = zdepth_msg->width;
     int height = zdepth_msg->height;
 
+    auto start_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     std::vector<uint16_t> depth;
     zdepth::DepthResult result = decompressor.Decompress(
       zdepth_msg->data, width, height, depth
     );
 
+    auto decompress_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     if (result != zdepth::DepthResult::Success) {
       ROS_ERROR("Failed to decompress");
       return;
     }
 
-    std::vector<uint8_t> depth_vec;
-    for (int i=0; i<depth.size(); ++i){
-       uint8_t* d = reinterpret_cast<uint8_t *>(&depth[i]);
-       std::vector<uint8_t> dv(d, d+2);
-       depth_vec.insert(depth_vec.end(), dv.begin(), dv.end());
-    }
+    uint8_t* depth_vec_beg = reinterpret_cast<uint8_t *>(&depth[0]);
+    uint8_t* depth_vec_end = depth_vec_beg + depth.size() * 2;
+    std::vector<uint8_t> depth_vec(depth_vec_beg, depth_vec_end);
+    auto copy_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+    // DEBUG
+    ROS_DEBUG_STREAM("decompress: " << decompress_time - start_time << "ms");
+    ROS_DEBUG_STREAM("copy      : " << copy_time - decompress_time << "ms");
 
     // callback
     sensor_msgs::Image::Ptr img_msg(new sensor_msgs::Image());
