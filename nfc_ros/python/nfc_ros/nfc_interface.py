ad36af6e623d5baeaa86064ec1c91f7d6f61b8c0
Koki Shinjo <sktometometo@gmail.com>
2022-12-02 12:16:46 +0900

    [nfc_ros] fix bugs

--- nfc_ros/python/nfc_ros/nfc_interface.py
+++ nfc_ros/python/nfc_ros/nfc_interface.py
@@ -4,8 +4,8 @@ import nfc
 class NFCInterface:
 
     def __init__(self,
-                 path = 'usb',
-                 targets = ['106A', '106B', '212F']):
+                 path='usb',
+                 targets=['106A', '106B', '212F']):
         """
         Args:
           path(str):
@@ -20,14 +20,23 @@ class NFCInterface:
 
         self.clf.close()
 
-    def read_card(self, targets=None):
+    def read_card(
+            self,
+            targets=None,
+            on_startup=lambda targets: targets,
+            on_discover=lambda target: True,
+            on_connect=lambda tag: False,
+            on_release=lambda tag: False):
 
         if targets is None:
             targets = self.targets
         tag = self.clf.connect(
             rdwr={
                 'targets': targets,
-                'on-connect': lambda tag: False
+                'on-startup': on_startup,
+                'on-discover': on_discover,
+                'on-connect': on_connect,
+                'on-release': on_release,
             }
         )
         return tag
--- nfc_ros/python/nfc_ros/nfc_ros_node.py
+++ nfc_ros/python/nfc_ros/nfc_ros_node.py
@@ -1,5 +1,8 @@
 import nfc
 import nfc.tag
+import nfc.tag.tt3
+import nfc.tag.tt3_sony
+import nfc.tag.tt4
 
 import rospy
 from nfc_ros.msg import Tag
@@ -13,32 +16,41 @@ class NFCROSNode:
 
         self.interface = NFCInterface()
         self.pub_tag = rospy.Publisher('~tag', Tag, queue_size=1)
+        rospy.loginfo('Initialized')
+
+    def publish_tag(self, tag):
+
+        rospy.loginfo('tag: {}'.format(tag))
+        msg = Tag()
+        if type(tag.identifier) == bytes:
+            msg.identifier = tag.identifier.hex()
+        else:
+            msg.identifier = tag.identifier
+        if type(tag) == nfc.tag.tt3.Type3Tag:
+            msg.tag_type = Tag.NFC_TAG_TYPE3TAG
+        elif type(tag) == nfc.tag.tt3_sony.FelicaStandard:
+            msg.tag_type == Tag.NFC_TAG_FelicaStandard
+        elif type(tag) == nfc.tag.tt3_sony.FelicaMobile:
+            msg.tag_type == Tag.NFC_TAG_FelicaMobile
+        elif type(tag) == nfc.tag.tt3_sony.FelicaLite:
+            msg.tag_type == Tag.NFC_TAG_FelicaLite
+        elif type(tag) == nfc.tag.tt3_sony.FelicaLiteS:
+            msg.tag_type == Tag.NFC_TAG_FelicaLiteS
+        elif type(tag) == nfc.tag.tt3_sony.FelicaPlug:
+            msg.tag_type == Tag.NFC_TAG_FelicaPlug
+        elif type(tag) == nfc.tag.tt4.Type4Tag:
+            msg.tag_type = Tag.NFC_TAG_TYPE4TAG
+        elif type(tag) == nfc.tag.tt4.Type4ATag:
+            msg.tag_type = Tag.NFC_TAG_TYPE4ATAG
+        elif type(tag) == nfc.tag.tt4.Type4BTag:
+            msg.tag_type = Tag.NFC_TAG_TYPE4BTAG
+        else:
+            rospy.logerr('Unknown tag type: {}'.format(type(tag)))
+            return True
+        self.pub_tag.publish(msg)
+        return True
 
     def spin(self):
 
         while not rospy.is_shutdown():
-            tag = self.interface.read_card()
-            msg = Tag()
-            msg.identifier = tag.identifier
-            #if type(tag) == nfc.tag.tt3.Type3Tag:
-            #    msg.tag_type = Tag.NFC_TAG_TYPE3TAG
-            #elif type(tag) == nfc.tag.tt3_sony.FelicaStandard:
-            #    msg.tag_type == Tag.NFC_TAG_FelicaStandard
-            #elif type(tag) == nfc.tag.tt3_sony.FelicaMobile:
-            #    msg.tag_type == Tag.NFC_TAG_FelicaMobile
-            #elif type(tag) == nfc.tag.tt3_sony.FelicaLite:
-            #    msg.tag_type == Tag.NFC_TAG_FelicaLite
-            #elif type(tag) == nfc.tag.tt3_sony.FelicaLiteS:
-            #    msg.tag_type == Tag.NFC_TAG_FelicaLiteS
-            #elif type(tag) == nfc.tag.tt3_sony.FelicaPlug:
-            #    msg.tag_type == Tag.NFC_TAG_FelicaPlug
-            #elif type(tag) == nfc.tag.tt3.Type4Tag:
-            #    msg.tag_type = Tag.NFC_TAG_TYPE4TAG
-            #elif type(tag) == nfc.tag.tt3.Type4ATag:
-            #    msg.tag_type = Tag.NFC_TAG_TYPE4ATAG
-            #elif type(tag) == nfc.tag.tt3.Type4BTag:
-            #    msg.tag_type = Tag.NFC_TAG_TYPE4BTAG
-            #else:
-            #    rospy.logerr('Unknown tag type: {}'.format(type(tag)))
-            #    continue
-            self.pub_tag.publish(msg)
+            self.interface.read_card(on_connect=self.publish_tag)
