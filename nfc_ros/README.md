753e928db40a0247dffae47fb1289be7bb002cf1
Koki Shinjo <sktometometo@gmail.com>
2022-12-07 12:45:41 +0900

    [nfc_ros] add documentation, support ndef

--- nfc_ros/README.md
+++ nfc_ros/README.md
@@ -1,3 +1,27 @@
 # nfc_ros
 
 This package is for nfc communication.
+
+## How to use
+
+Before use this, please install nfcpy and setup udev for your device.
+
+```bash
+pip3 install nfcpy
+```
+
+And then, connect your NFCReader to PC and run
+
+```bash
+python3 -m nfc
+```
+
+This will show messages and if you need to do somethin, there will be an instruction. please follow that.
+
+After that, please build this package and run.
+
+```bash
+roslaunch nfc_ros demo.launch
+```
+
+If you want to customize your NFC Tag, please use other tools (e.g. NFC Tools [for Android](https://play.google.com/store/apps/details?id=com.wakdev.wdnfc&hl=ja&gl=US&pli=1) and [for iPhone](https://apps.apple.com/jp/app/nfc-tools/id1252962749). )
--- nfc_ros/python/nfc_ros/nfc_ros_node.py
+++ nfc_ros/python/nfc_ros/nfc_ros_node.py
@@ -1,11 +1,16 @@
 import nfc
 import nfc.tag
+import nfc.tag.tt1
+import nfc.tag.tt1_broadcom
+import nfc.tag.tt2
+import nfc.tag.tt2_nxp
 import nfc.tag.tt3
 import nfc.tag.tt3_sony
 import nfc.tag.tt4
 
 import rospy
 from nfc_ros.msg import Tag
+from nfc_ros.msg import NDEFRecord
 
 from nfc_ros.nfc_interface import NFCInterface
 
@@ -22,11 +27,19 @@ class NFCROSNode:
 
         rospy.loginfo('tag: {}'.format(tag))
         msg = Tag()
+
         if type(tag.identifier) == bytes:
             msg.identifier = tag.identifier.hex()
         else:
             msg.identifier = tag.identifier
-        if type(tag) == nfc.tag.tt3.Type3Tag:
+
+        if type(tag) == nfc.tag.tt1.Type1Tag:
+            msg.tag_type = Tag.NFC_TAG_TYPE1TAG
+        elif type(tag) == nfc.tag.tt2.Type2Tag:
+            msg.tag_type = Tag.NFC_TAG_TYPE2TAG
+        elif type(tag) == nfc.tag.tt2_nxp.NTAG215:
+            msg.tag_type = Tag.NFC_TAG_TYPE2NXPNTAG215
+        elif type(tag) == nfc.tag.tt3.Type3Tag:
             msg.tag_type = Tag.NFC_TAG_TYPE3TAG
         elif type(tag) == nfc.tag.tt3_sony.FelicaStandard:
             msg.tag_type == Tag.NFC_TAG_FelicaStandard
@@ -47,6 +60,15 @@ class NFCROSNode:
         else:
             rospy.logerr('Unknown tag type: {}'.format(type(tag)))
             return True
+
+        if tag.ndef is not None:
+            for record in tag.ndef.records:
+                ndef_record = NDEFRecord()
+                ndef_record.type = record.type
+                ndef_record.name = record.name
+                ndef_record.data = record.data.decode()
+                msg.ndef_records.append(ndef_record)
+
         self.pub_tag.publish(msg)
         return True
 
