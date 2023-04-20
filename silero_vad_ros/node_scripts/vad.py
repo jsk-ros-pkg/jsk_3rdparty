ef0bf964d0aedd2062ec76f5ba0a815a895f084a
Koki Shinjo <sktometometo@gmail.com>
2023-04-20 19:21:16 +0900

    [silero_vad_ros] update

--- silero_vad_ros/node_scripts/vad.py
+++ silero_vad_ros/node_scripts/vad.py
@@ -13,28 +13,32 @@ class SileroVADROS(VADBaseNode):
 
   def __init__(self):
 
+    model_vad, _ = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                                  model='silero_vad',
+                                  force_reload=True)
+    self.model_vad = model_vad
+
     super(SileroVADROS, self).__init__()
 
-    self.model_vad, _ = torch.hub.load(repo_or_dir='snakers4/silero-vad',
-                                       model='silero_vad',
-                                       force_reload=True)
+    rospy.loginfo('Initialized.')
 
   def _get_vad_confidence(self, chunk, sampling_rate):
-    return self.model_vad(
-        torch.from_numpy(self._convert_bytearray_to_numpy_array(chunk)),
-        sampling_rate).item()
+    print('chunk: {}'.format(chunk))
+    nparray = self._convert_bytearray_to_numpy_array(chunk)
+    print('nparray: {}'.format(nparray))
+    return self.model_vad(torch.from_numpy(nparray), sampling_rate).item()
 
   def _convert_bytearray_to_numpy_array(self, data):
     if self._audio_info.sample_format == 'S16LE':
-      return np.array(struct.unpack("{}h".format(len(data) / 2), data))
+      return np.array(struct.unpack("{}h".format(int(len(data) / 2)), data))
     else:
-      return np.array([])
+      raise ValueError()
 
 
 def main():
 
-  rospy.init_node('_ros')
-  node = WebRTCVADROS()
+  rospy.init_node('silero_vad_ros')
+  node = SileroVADROS()
   rospy.spin()
 
 
