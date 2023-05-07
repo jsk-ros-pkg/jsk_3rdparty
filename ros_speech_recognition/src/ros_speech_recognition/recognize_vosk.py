e6956bc77911dd73a42f7e3ac0d45d077a0957ef
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2023-05-07 14:17:18 +0900

    [ros_speech_recognition] Use vosk engine

---
+++ ros_speech_recognition/src/ros_speech_recognition/recognize_vosk.py
@@ -0,0 +1,26 @@
+# file to override recognize_vosk
+# we need this to use vosk model anywhere
+
+from speech_recognition import *
+from ros_speech_recognition.recognize_google_cloud import RecognizerEx
+from vosk import Model, KaldiRecognizer
+
+def recognize_vosk(self, audio_data, model_path=None, language='en'):
+
+    assert isinstance(audio_data, AudioData), "Data must be audio data"
+
+    if not hasattr(self, 'vosk_model'):
+        if model_path is not None:
+            self.vosk_model = Model(model_path)
+        else:
+            print("Please download the model from https://alphacephei.com/vosk/models and specify its path as 'vosk_model_path'.")
+            exit (1)
+
+    rec = KaldiRecognizer(self.vosk_model, 16000);
+
+    rec.AcceptWaveform(audio_data.get_raw_data(convert_rate=16000, convert_width=2));
+    finalRecognition = rec.FinalResult()
+
+    return finalRecognition
+
+RecognizerEx.recognize_vosk = recognize_vosk
