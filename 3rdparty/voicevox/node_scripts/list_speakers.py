a0461b5defc9a99acbcb9717e9081126aaeefdce
Kei <kei.okada@gmail.com>
2025-04-24 23:47:18 +0900

    cleanup code, use 0.15.7 core and 0.14.7 engine for chibisiki

---
+++ 3rdparty/voicevox/node_scripts/list_speakers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from voicevox import Client
+import asyncio
+
+
+async def main():
+    async with Client() as client:
+        # check core
+        for version in await client.fetch_core_versions():
+            print("Core version: {}".format(version))
+        # check engine
+        engine_version = await client.fetch_engine_version()
+        print("Engine version: {}".format(engine_version))
+        # check device
+        for device in await client.http.request("GET", "/supported_devices"):
+            print("Device: {}".format(device))
+        # check speaker
+        for speaker in await client.fetch_speakers():
+            print(speaker.uuid, speaker.name, speaker.supported_features.permitted_synthesis_morphing)
+            for styles in speaker.styles:
+                print(styles.id, speaker.name, styles.name)
+
+if __name__ == "__main__":
+    asyncio.run(main())
