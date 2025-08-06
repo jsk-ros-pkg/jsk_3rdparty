30afe3419f67d363d1eaf42ec24ee3fbf912cfa3
Shun Hasegawa <pazeshun3684@gmail.com>
2025-08-06 21:16:25 +0900

    [power_switching_tools_ros] Support USB PPPS hubs

---
+++ power_switching_tools_ros/node_scripts/usb_ppps_hub_driver.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+from power_switching_tools_ros.power_switching_driver_base import PowerSwitchingDriverBase
+import rospy
+import subprocess
+
+
+class UsbPppsHubDriver(PowerSwitchingDriverBase):
+
+    def __init__(self):
+        try:
+            self.hub_loc = str(rospy.get_param('~hub_location'))
+            self.hub_port = str(rospy.get_param('~hub_port'))
+        except KeyError as e:
+            rospy.logfatal(
+                '[{}] Please set hub_location and hub_port'.format(
+                    rospy.get_name()))
+            rospy.signal_shutdown('Could not get required params')
+            return
+
+        try:
+            res = subprocess.run(
+                ['uhubctl',
+                 '-l', self.hub_loc,
+                 '-p', self.hub_port],
+                capture_output=True,
+                text=True,
+            )
+        except FileNotFoundError as e:
+            rospy.logfatal(
+                '[{}] Please install uhubctl '
+                'by "sudo apt install uhubctl"'.format(
+                    rospy.get_name()))
+            rospy.signal_shutdown('uhubctl is not installed')
+            return
+        if ((res.returncode == 0
+             and 'Port {}:'.format(self.hub_port) not in res.stdout)
+            or ('Bad port spec' in res.stderr)):
+            rospy.logfatal(
+                '[{}] hub_port {} is invalid'.format(
+                    rospy.get_name(), self.hub_port))
+            rospy.signal_shutdown('hub_port is invalid')
+            return
+        if res.returncode != 0:
+            if ('No compatible smart hubs '
+                'detected at location {}'.format(
+                    self.hub_loc) in res.stderr):
+                rospy.logfatal(
+                    '[{}] hub_location {} is invalid'.format(
+                        rospy.get_name(), self.hub_loc))
+                rospy.signal_shutdown('hub_location is invalid')
+                return
+            else:
+                rospy.logfatal(
+                    '[{}] Unknown error'.format(
+                        rospy.get_name()))
+                rospy.signal_shutdown('Unknown error')
+                return
+
+        super(UsbPppsHubDriver, self).__init__()
+
+    def _pwr(self, is_on):
+        res = subprocess.run(
+            ['uhubctl',
+             '-a', str(int(is_on)),
+             '-l', self.hub_loc,
+             '-p', self.hub_port],
+            capture_output=True,
+            text=True,
+        )
+        if res.returncode == 0:
+            rospy.loginfo(
+                '[{}] Changed power state to {}'.format(
+                    rospy.get_name(), is_on))
+            return True
+        else:
+            rospy.logerr(
+                '[{}] uhubctl returns an error. '
+                'Check if hub is connected'.format(
+                    rospy.get_name()))
+            rospy.logdebug(
+                '[{}] Error message returned from uhubctl: {}'.format(
+                    rospy.get_name(), res.stderr))
+            return False
+
+
+if __name__ == '__main__':
+    rospy.init_node('usb_ppps_hub_driver')
+    app = UsbPppsHubDriver()
+    rospy.spin()
